d280e00582387f28ded641712d48e3379d407a2d
choimyungbin <mbchoi1133@naver.com>
2022-02-11 05:10:34 +0900

    Add Event

---
+++ app/src/main/java/com/appdong/constraintlayout/DrawableActivity.kt
@@ -0,0 +1,17 @@
+package com.appdong.constraintlayout
+
+import android.os.Bundle
+import android.widget.RadioButton
+
+class DrawableActivity {
+
+	var radioButton : RadioButton? = null
+	var checked : Boolean? = null
+	//main 아니라서 override 불가, 일시적으로 주석처리함
+//	override fun onCreate(savedInstanceState: Bundle?) {
+//		super.onCreate(savedInstanceState)
+//		setContentView(R.layout.activity_main)
+//
+//
+//	}
+}
