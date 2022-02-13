a9c2e3dcd5e6aa080abea8643968147df0995448
choimyungbin <mbchoi1133@naver.com>
2022-02-13 22:26:18 +0900

    Add Toast and SnackBar

--- app/src/main/java/com/appdong/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/appdong/constraintlayout/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.appdong.constraintlayout
 
 import android.os.Bundle
+import android.text.Layout
 import android.view.Gravity
 import android.view.ViewGroup
 import android.widget.Button
@@ -15,14 +16,14 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-		val button = findViewById<Button>(R.id.toastbutton)
+		val button = findViewById<Button>(R.id.toastButton)
 		button.setOnClickListener { v ->
 			val toast = makeText(this, "위치가 바뀐 토스트", Toast.LENGTH_LONG)
-			toast.setGravity(Gravity.TOP or Gravity.LEFT, 200, 200)
+			toast.setGravity(Gravity.TOP or Gravity.START, 200, 200)
 			toast.show()
 		}
-		val button2 = findViewById<Button>(R.id.changebutton)
-		button.setOnClickListener { v ->
+		val button2 = findViewById<Button>(R.id.changeButton)
+		button2.setOnClickListener { v ->
 			val inflater = layoutInflater
 			val layout = inflater.inflate(R.layout.toastborder, findViewById(R.id.toast_layout_root))
 			val text = layout.findViewById<TextView>(R.id.textToast)
@@ -35,9 +36,10 @@ class MainActivity : AppCompatActivity() {
 
 			toast.show()
 		}
-		val button3 = findViewById<Button>(R.id.button3)
+		val button3 = findViewById<Button>(R.id.button20)
 		button3.setOnClickListener { v ->
-			Snackbar.make(v, "스낵바입니다", Snackbar.LENGTH_LONG).show()
+			var snackbar = Snackbar.make(v, "스낵바입니다", Snackbar.LENGTH_LONG)
+			snackbar.show()
 		}
 	}
 
